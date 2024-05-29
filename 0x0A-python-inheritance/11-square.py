9a8a5cc2b3f208b309695f3218463add2ac3066c
EliezerSunny <adetunjieliazer@gmail.com>
2024-05-29 10:50:45 -0700

    modified

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""Defines Rectangle module."""
+"""Further building of the square class"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """class body."""
-
+    """Define inhereted BaseGeometry class Rectangle"""
     def __init__(self, size):
-
+        "Initialize"
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Return the rectangle description"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
