f0a39e3517ff4f942e10e99dbc7737a2e802b8d1
EliezerSunny <adetunjieliazer@gmail.com>
2024-06-19 15:26:05 -0700

    modified

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const stringList = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const stringList = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const i in stringList) {
   console.log(stringList[i]);
 }
