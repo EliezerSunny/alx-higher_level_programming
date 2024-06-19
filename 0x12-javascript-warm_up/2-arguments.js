8a179045e6abc640ade8e5ec370796cd97896595
EliezerSunny <adetunjieliazer@gmail.com>
2024-06-19 15:02:47 -0700

    modified

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const argCount = process.argv.length;
-if (argCount === 0) {
-  console.log('No argument');
-} else if (argCount === 1) {
-  console.log('Argument found');
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
 } else {
-  console.log('Arguments found');
+  console.log("Arguments found");
 }
