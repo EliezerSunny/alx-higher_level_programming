cfd85a22d6ac6eb82bbe8296e02d097d6abc9866
EliezerSunny <adetunjieliazer@gmail.com>
2024-06-22 16:16:05 -0700

    modified

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""
-Script that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument.
-"""
+"""Script to display all rows of states table where name matches argument"""
+
 import MySQLdb
 import sys
 
