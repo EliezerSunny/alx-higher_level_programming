f54d9c55e9e70bfd3a704561437a1755da809845
EliezerSunny <adetunjieliazer@gmail.com>
2024-06-22 16:40:32 -0700

    modified

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,28 +1,36 @@
 #!/usr/bin/python3
 """
-Script that takes state name as an argument and lists all cities from
-that state using database hbtn_0e_4_usa.
+Query to retrieve all cities of a given state from the cities table
+in hbtn_0e_4_usa database.
 """
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    # Check if all arguments are provided
+    if len(sys.argv) != 5:
+        print("Usage: ./5-filter_cities.py <username> <password> <database_name> <state_name>")
+        sys.exit(1)
+
+    username, password, database, state_name = sys.argv[1:5]
+
     try:
         # Connect to MySQL database
-        db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], database=sys.argv[3], host="localhost", port=3306)
-        cur = db.cursor()
+        db = MySQLdb.connect(user=username, passwd=password, db=database, host="localhost", port=3306)
+        cursor = db.cursor()
 
-        # Execute the SQL query to retrieve cities of the given state
-        query = ("SELECT cities.`name` FROM cities "
-                 "JOIN states ON cities.`state_id` = states.`id` "
-                 "WHERE states.`name` = %s "
-                 "COLLATE 'latin1_general_cs' "
-                 "ORDER BY cities.`id` ASC")
-        cur.execute(query, (sys.argv[4],))
+        # Execute the SQL query with JOIN to retrieve cities of the given state
+        query = ("SELECT cities.name "
+                 "FROM cities "
+                 "JOIN states ON cities.state_id = states.id "
+                 "WHERE states.name = %s "
+                 "ORDER BY cities.id ASC")
+        cursor.execute(query)
 
         # Fetch all rows and join city names into a comma-separated string
-        cities_list = ', '.join(map(lambda x: x[0], cur.fetchall()))
+        results = cursor.fetchall()
+        cities_list = ", ".join(row[0] for row in results)
 
         # Print the list of cities
         print(cities_list)
@@ -32,7 +40,7 @@ if __name__ == "__main__":
 
     finally:
         # Close cursor and database connection in finally block
-        if cur:
-            cur.close()
+        if cursor:
+            cursor.close()
         if db:
             db.close()
