6deee0746cafb21a523888fefe26fdca03719dc6
EliezerSunny <adetunjieliazer@gmail.com>
2024-06-22 17:36:45 -0700

    modified

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,46 +1,28 @@
 #!/usr/bin/python3
 """
-Query to retrieve all cities of a given state from the cities table
-in hbtn_0e_4_usa database.
+Script that takes in the name of a state as an argument and lists
+all cities of that state, using the database
 """
-
 import MySQLdb
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-    # Check if all arguments are provided
-    if len(sys.argv) != 5:
-        print("Usage: ./5-filter_cities.py <username> <password> <database_name> <state_name>")
-        sys.exit(1)
+# The code should not be executed when imported
+if __name__ == '__main__':
+    # make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
 
-    username, password, database, state_name = sys.argv[1:5]
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                WHERE states.name = %s", [argv[4]])
 
-    try:
-        # Connect to MySQL database
-        db = MySQLdb.connect(user=username, passwd=password, db=database, host="localhost", port=3306)
-        cursor = db.cursor()
+    rows = cur.fetchall()
+    j = []
+    for i in rows:
+        j.append(i[1])
+    print(", ".join(j))
 
-        # Execute the SQL query with JOIN to retrieve cities of the given state
-        query = ("SELECT cities.name "
-                 "FROM cities "
-                 "JOIN states ON cities.state_id = states.id "
-                 "WHERE states.name = %s "
-                 "ORDER BY cities.id ASC")
-        cursor.execute(query, (state_name,))
-
-        # Fetch all rows and join city names into a comma-separated string
-        results = cursor.fetchall()
-        cities_list = ", ".join(row[0] for row in results)
-
-        # Print the list of cities
-        print(cities_list)
-
-    except MySQLdb.Error as e:
-        print(f"Error connecting to MySQL DB: {e}")
-
-    finally:
-        # Close cursor and database connection in finally block
-        if cursor:
-            cursor.close()
-        if db:
-            db.close()
+    # Clean up process
+    cur.close()
+    db.close()
