d7ef27271f71139f07fd6b286458750dd9df83e0
EliezerSunny <adetunjieliazer@gmail.com>
2024-06-22 17:40:17 -0700

    modified

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,43 +1,25 @@
 #!/usr/bin/python3
 """
-Script that lists all State objects from the database hbtn_0e_6_usa.
+Script that lists all State objects from the database - Using module SQLAlchemy
 """
-
-import sys
+from model_state import Base, State
+from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 if __name__ == "__main__":
-    # Check if all arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql username> <mysql password> <database name>".format(sys.argv[0]))
-        sys.exit(1)
 
-    # Capture arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-    # Create engine to connect to MySQL database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(mysql_username, mysql_password, database_name))
-
-    # Bind the engine to the Base class
+    # create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+    # create a Session
+    session = Session()
     Base.metadata.create_all(engine)
 
-    # Create a configured "Session" class
-    Session = sessionmaker(bind=engine)
-
-    # Create a Session instance
-    session = Session()
-
-    # Query all State objects and sort by id
-    states = session.query(State).order_by(State.id).all()
-
-    # Print results
-    for state in states:
+    s_tate = session.query(State).order_by(State.id).all()
+    for state in s_tate:
         print("{}: {}".format(state.id, state.name))
-
-    # Close the session
     session.close()
