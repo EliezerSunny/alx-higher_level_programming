dbd752abf0028c7d5300c36fce189e73ec4c7b0b
EliezerSunny <adetunjieliazer@gmail.com>
2024-06-22 17:09:08 -0700

    modified

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -10,12 +10,10 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    # Check if correct number of arguments are provided
     if len(sys.argv) != 5:
         print("Usage: {} username password database_name state_name".format(sys.argv[0]))
         sys.exit(1)
 
-    # Arguments from command line
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
@@ -28,11 +26,11 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query to find the state
-    state = session.query(State).filter(State.name == state_name).first()
+    # Query for the state
+    result = session.query(State).filter(State.name == state_name).first()
 
-    if state:
-        print(state.id)
+    if result:
+        print(result.id)
     else:
         print("Not found")
 
