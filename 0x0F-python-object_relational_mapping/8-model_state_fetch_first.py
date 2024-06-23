ed83df9d96ed4a227081eb0e591246261ab5bb3d
EliezerSunny <adetunjieliazer@gmail.com>
2024-06-22 17:41:30 -0700

    modified

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,45 +1,26 @@
 #!/usr/bin/python3
 """
-Script that prints the first State object from the database hbtn_0e_6_usa.
+Script that prints the first State object from the database
 """
 
-import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from sys import argv
 
 if __name__ == "__main__":
-    # Check if all arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql username> <mysql password> <database name>".format(sys.argv[0]))
-        sys.exit(1)
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-    # Capture arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+    Session = sessionmaker()
+    session = Session(bind=engine)
 
-    # Create engine to connect to MySQL database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(mysql_username, mysql_password, database_name))
-
-    # Bind the engine to the Base class
     Base.metadata.create_all(engine)
+    s_tate = session.query(State).order_by(State.id).first()
 
-    # Create a configured "Session" class
-    Session = sessionmaker(bind=engine)
-
-    # Create a Session instance
-    session = Session()
-
-    # Query the first State object
-    first_state = session.query(State).order_by(State.id).first()
-
-    # Check if result exists and print accordingly
-    if not first_state:
-        print("Nothing")
+    if s_tate:
+        print("{}: {}".format(s_tate.id, s_tate.name))
     else:
-        print("{}: {}".format(first_state.id, first_state.name))
-
-    # Close the session
+        print("Nothing")
     session.close()
