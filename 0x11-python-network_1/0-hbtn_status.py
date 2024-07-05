22f5298cb29ad78cf8d14e28b8a1f1b20b97b653
EliezerSunny <adetunjieliazer@gmail.com>
2024-07-05 15:37:29 -0700

    modified

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
+"""Fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         r = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(r)))
         print("\t- content: {}".format(r))
-        print("\t- utf8 content: {}".format(r.decode("UTF-8")))
+        print("\t- utf8 content: {}".format(r.decode("utf-8")))
