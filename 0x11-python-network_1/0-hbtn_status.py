034e049d3df809773ddaae971172130fe0f9a1b0
EliezerSunny <adetunjieliazer@gmail.com>
2024-07-05 15:34:09 -0700

    modified

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
 """Fetches https://intranet.hbtn.io/status"""
-
 import urllib.request
-import urllib.error
 
 if __name__ == "__main__":
-    try:
-        with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-            r = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(r)))
-            print("\t- content: {}".format(r))
-            print("\t- utf8 content: {}".format(r.decode("UTF-8")))
-    except urllib.error.URLError as e:
-        print(f"Error fetching URL: {e}")
-
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        r = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(r)))
+        print("\t- content: {}".format(r))
+        print("\t- utf8 content: {}".format(r.decode("UTF-8")))
